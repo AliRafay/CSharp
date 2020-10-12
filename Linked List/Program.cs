dfb50e662ec1d220ad83f57256efa02340cbbdc9
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-12 18:36:16 +0500

    AddToEnd() created
    AddToEnd() created

---
+++ Linked List/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Linked_List
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int i)
+        {
+            data = i;
+            next = null;
+        }
+
+        public void Print()
+        {
+            Console.Write("|" + data + "|-->");
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+            }
+            else
+            {
+                next.AddToEnd(data);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node myNode = new Node(5);
+            //myNode.next = new Node(3);
+            //myNode.next.next = new Node(9);
+            myNode.Print();
+        }
+    }
+}
