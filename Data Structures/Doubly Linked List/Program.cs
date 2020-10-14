f025f81fcb041c275230fde9c4fe0a43283ef88e
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-14 13:48:34 +0500

    AddToEnd() and AddToBeginning()

--- Data Structures/Doubly Linked List/Program.cs
+++ Data Structures/Doubly Linked List/Program.cs
@@ -5,107 +5,99 @@ namespace Doubly_Linked_List
     public class Node
     {
         public int data;
-        public Node flink;
-        public Node blink;
+        public Node next;
+        public Node previous;
 
         public Node(int i)
         {
             data = i;
-            flink = null;
-            blink = null;
+            next = null;
+            previous = null;
         }
 
         public void Print()
         {
             Console.Write("|" + data + "|-->");
-            if (flink != null)
+            if (next != null)
             {
-                flink.Print();
+                next.Print();
             }
         }
 
         public void AddToEnd(int data)
         {
-            if (flink == null)
+            if (next == null)
             {
-                flink = new Node(data);
-                flink.blink = this;
+                next = new Node(data);
+                next.previous = this;
+
             }
             else
             {
-                flink.AddToEnd(data);
+                next.AddToEnd(data);
             }
+
         }
 
-        public void AddToBeginning(int data)   // error NOT ADDING TO BEGINNING
+        public class DoublyLinkedList
         {
-            if (blink == null)
+            public Node headNode;
+
+            public DoublyLinkedList()
             {
-                blink = new Node(data);
-                blink.flink = this;
+                headNode = null;
             }
-            else
+            public void Print()
             {
-                blink.AddToBeginning(data);
+                if (headNode != null)
+                {
+                    headNode.Print();
+                }
             }
+            public void AddToEnd(int data)
+            {
+                if (headNode == null)
+                {
+                    headNode = new Node(data);
+                }
+                else
+                {
+                    headNode.AddToEnd(data);
+                }
+            }
+
+            public void AddToBeginning(int data)
+            {
+                if (headNode == null)
+                {
+                    headNode = new Node(data);
+                }
+                else
+                {
+                    Node temp = new Node(data);
+                    temp.next = headNode;
+                    headNode = temp;
+                }
+            }
+
+
         }
 
-    }
-
-    public class DoublyLinkedList
-    {
-        public Node headNode;
-
-        public DoublyLinkedList()
+        class Program
         {
-            headNode = null;
-        }
-        public void Print()
-        {
-            if (headNode != null)
+            static void Main(string[] args)
             {
-                headNode.Print();
+                DoublyLinkedList myList = new DoublyLinkedList();
+
+                myList.AddToEnd(9);
+                myList.AddToEnd(17);
+                myList.AddToEnd(40);
+
+                myList.AddToBeginning(7);
+
+                myList.Print();
+
             }
-        }
-        public void AddToEnd(int data)
-        {
-            if (headNode == null)
-            {
-                headNode = new Node(data);
-            }
-            else
-            {
-                headNode.AddToEnd(data);
-            }
-        }
-
-        public void AddToBeginning(int data)
-        {
-            if (headNode == null)
-            {
-                headNode = new Node(data);
-            }
-            else
-            {
-                headNode.AddToBeginning(data);
-            }
-        }
-
-
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DoublyLinkedList myList = new DoublyLinkedList();
-
-            myList.AddToEnd(9);
-            myList.AddToEnd(40);
-            myList.AddToBeginning(7);
-
-            myList.Print();
-
         }
     }
 }
