863f589392a14cb5ecab0ccdf5f2462ac5d64490
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-15 14:05:46 +0500

    Stacks Implemented

--- Data Structures/Stacks/Program.cs
+++ Data Structures/Stacks/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Stacks
 {
+
+    class Stack
+    {
+        private int p_index;
+        private List<string> list = new List<string>();
+        public Stack()
+        {
+            p_index = -1;
+        }
+        public int count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+        public void Push(string item)
+        {
+            list.Add(item);
+            p_index++;
+        }
+        public object Pop()
+        {
+            string temp = list[p_index];
+            list.RemoveAt(p_index);
+            p_index--;
+            return temp;
+        }
+        public void Clear()
+        {
+            list.Clear();
+            p_index = -1;
+        }
+        public object Peek()
+        {
+            return list[p_index];
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack myStack = new Stack();
+            myStack.Push("HELLO");
+            myStack.Push("WORLD");
+            Console.WriteLine(myStack.count);
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Peek());
+            myStack.Clear();
+            Console.WriteLine(myStack.count);
+
         }
     }
 }
