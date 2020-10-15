c9be784b29876fe37b424814d2c6d612b4eaa2c7
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-15 15:00:51 +0500

    Queue Implemented

--- Data Structures/Queues/Program.cs
+++ Data Structures/Queues/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Queues
 {
+    public class Queue
+    {
+        private List<int> pqueue = new List<int>();
+        public void EnQueue(int item)
+        {
+            pqueue.Add(item);
+        }
+        public void DeQueue()
+        {
+            pqueue.RemoveAt(0);
+        }
+        public object Peek()
+        {
+            return pqueue[0];
+        }
+        public void ClearQueue()
+        {
+            pqueue.Clear();
+        }
+        public int Count()
+        {
+            return pqueue.Count;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue myQueue = new Queue();
+            myQueue.EnQueue(7);
+            myQueue.EnQueue(2);
+            myQueue.EnQueue(13);
+            Console.WriteLine("Count: "+myQueue.Count());
+            Console.WriteLine("Peek: "+myQueue.Peek());
+            myQueue.DeQueue();
+            myQueue.DeQueue();
+            Console.WriteLine("Peek: "+myQueue.Peek());
+            Console.WriteLine("Count: "+myQueue.Count());
+            myQueue.ClearQueue();
+            Console.WriteLine("Count: "+myQueue.Count());
+
         }
     }
 }
