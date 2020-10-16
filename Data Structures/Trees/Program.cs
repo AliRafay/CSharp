5def703ad2ab68675e191ab8f0237c353c727928
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-16 17:40:36 +0500

    InOrder(),PreOrder() and PostOrder() Treversals

--- Data Structures/Trees/Program.cs
+++ Data Structures/Trees/Program.cs
@@ -4,26 +4,53 @@ namespace Trees
 {
     public class Node
     {
-        public int Data;
+        public int data;
         public Node left;
         public Node right;
         public void DisplayNode()
         {
-            Console.Write(Data + "  ");
+            Console.Write(data + "  ");
+        }
+        public void InOrderTreversal(Node Current)
+        {
+            if (Current != null)
+            {
+                InOrderTreversal(Current.left);
+                Current.DisplayNode();
+                InOrderTreversal(Current.right);
+            }
+        }
+        public void PostOrderTreversal(Node Current)
+        {
+            if (Current != null)
+            {
+                PostOrderTreversal(Current.left);
+                PostOrderTreversal(Current.right);
+                Current.DisplayNode();
+            }
+        }
+        public void PreOrderTreversal(Node Current)
+        {
+            if (Current != null)
+            {
+                Current.DisplayNode();
+                PreOrderTreversal(Current.left);
+                PreOrderTreversal(Current.right);
+            }
         }
     }
 
-    public class BinaryTree
+    public class BinarySearchTree
     {
         public Node root;
-        public BinaryTree()
+        public BinarySearchTree()
         {
             root = null;
         }
         public void Insert(int i)
         {
             Node myNode = new Node();
-            myNode.Data = i;
+            myNode.data = i;
             if (root == null)
             {
                 root = myNode;
@@ -35,7 +62,7 @@ namespace Trees
                 while (true)
                 {
                     parent = current;
-                    if (i < current.Data)
+                    if (i < current.data)
                     {
                         current = current.left;
                         if (current == null)
@@ -43,24 +70,77 @@ namespace Trees
                             parent.left = myNode;
                             break;
                         }
-                        else
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
                         {
-                            current = current.right;
-                            if (current == null)
-                            {
-                                parent.right = myNode;
-                                break;
-                            }
+                            parent.right = myNode;
+                            break;
                         }
                     }
                 }
-            }
-        }
-    class Program
-        {
-            static void Main(string[] args)
-            {
 
             }
         }
+        public void InOrderTreversal()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("The Tree is Empty");
+            }
+            else
+            {
+                Console.WriteLine("InOrder Treversal");
+                root.InOrderTreversal(root);
+                Console.WriteLine("\n");
+            }
+        }
+        public void PreOrderTreversal()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("The Tree is Empty");
+            }
+            else
+            {
+                Console.WriteLine("PreOrder Treversal");
+                root.PreOrderTreversal(root);
+                Console.WriteLine("\n");
+
+            }
+        }
+        public void PostOrderTreversal()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("The Tree is Empty");
+            }
+            else
+            {
+                Console.WriteLine("PostOrder Treversal");
+                root.PostOrderTreversal(root);
+                Console.WriteLine("\n");
+
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                BinarySearchTree myTree = new BinarySearchTree();
+                myTree.Insert(10);
+                myTree.Insert(15);
+                myTree.Insert(18);
+                myTree.Insert(8);
+                myTree.Insert(20);
+                myTree.Insert(12);
+
+                myTree.InOrderTreversal();
+                myTree.PreOrderTreversal();
+                myTree.PostOrderTreversal();
+            }
+        }
     }
+}
