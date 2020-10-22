be9c69bb9a7d99cb0fa5e649c30366c8e7b3398b
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-22 19:02:34 +0500

    Pangram Completed

---
+++ Code Force/Pangram/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+//https://codeforces.com/problemset/problem/520/A
+
+namespace Pangram
+{
+    class Program
+    {
+
+        static bool CheckPangram(int length, string sentence)
+        {
+            char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            List<char> alphabetsList = new List<char>(alphabets);
+            if (sentence.Length > 25)
+            {
+                for(int i = 0; i < sentence.Length ; i++)
+                {
+                    char character = sentence[i];
+                    if (alphabetsList.Contains(character))
+                    {
+                        alphabetsList.Remove(character);
+                    }
+                }
+                if(alphabetsList.Count == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Length of string: ");
+            int length = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Sentence: ");
+            string sentence = Console.ReadLine().ToLower();
+            if (CheckPangram(length, sentence))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+
+            }
+        }
+    }
+}
