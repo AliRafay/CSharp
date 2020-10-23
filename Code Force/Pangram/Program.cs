11bd890bc42ab124869bbc5792f3c5748ed6647e
Ali Rafay <64028495+AliRafay@users.noreply.github.com>
2020-10-23 14:00:19 +0500

    CostReducedPangram() in 62ms from 108ms

--- Code Force/Pangram/Program.cs
+++ Code Force/Pangram/Program.cs
@@ -7,14 +7,13 @@ namespace Pangram
 {
     class Program
     {
-
         static bool CheckPangram(int length, string sentence)
         {
             char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             List<char> alphabetsList = new List<char>(alphabets);
             if (sentence.Length > 25)
             {
-                for(int i = 0; i < sentence.Length ; i++)
+                for (int i = 0; i < sentence.Length; i++)
                 {
                     char character = sentence[i];
                     if (alphabetsList.Contains(character))
@@ -22,7 +21,7 @@ namespace Pangram
                         alphabetsList.Remove(character);
                     }
                 }
-                if(alphabetsList.Count == 0)
+                if (alphabetsList.Count == 0)
                 {
                     return true;
                 }
@@ -36,13 +35,44 @@ namespace Pangram
                 return false;
             }
         }
+        static bool ReducedCostPangram(string sentence)
+        {
+            bool isPangram = false;
+            if (sentence.Length > 25)
+            {
+                int[] arr = new int[26];
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    int Ascii = sentence[i];
+                    if (Ascii >= 'a')
+                    {
+                        arr[Ascii - 97]++;
+                    }
+                    else
+                    {
+                        arr[Ascii - 65]++;
+                    }
+                }
+
+                isPangram = true;
+                for (int i = 0; i < 26; i++)
+                {
+                    if (arr[i] == 0)
+                    {
+                        isPangram = false;
+                        break;
+                    }
+                }
+            }
+            return isPangram;
+        }
         static void Main(string[] args)
         {
             //Console.WriteLine("Enter Length of string: ");
             int length = int.Parse(Console.ReadLine());
             //Console.WriteLine("Enter Sentence: ");
-            string sentence = Console.ReadLine().ToLower();
-            if (CheckPangram(length, sentence))
+            string sentence = Console.ReadLine();
+            if (ReducedCostPangram(sentence))
             {
                 Console.WriteLine("YES");
             }
